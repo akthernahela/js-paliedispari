0e6016308ae244844c85a97fb8f340fe519b0ecc
akthernahela <nahela00@gmail.com>
2025-10-10 18:48:30 +0200

    Add isPalindroma function implementation

--- palidroma/main.js
+++ palidroma/main.js
@@ -1,6 +1,11 @@
 /*
-Palidroma
+Palindroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+function isPalindroma(word){
+    //Inverto e controllo se è uguale 
+    const wordReverse = word.split('').reverse().jion('');
+    return word === wordReverse;
+}
 
